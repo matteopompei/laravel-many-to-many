e9e3e5758e817de13d86ed3d7f76a156d68dca8f
Matteo Pompei <datxizzan@gmail.com>
2022-03-21 15:03:50 +0100

    first commit

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.admin')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Scrivi un nuovo post</div>
+
+          <div class="card-body">
+
+            <form action="{{ route('admin.posts.store') }}" method="post">
+              @csrf
+
+              <div class="form-group">
+                <label>Titolo</label>
+                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror"
+                  placeholder="Inserisci il titolo del post">
+              </div>
+
+              <div class="form-group">
+                <label>Testo</label>
+                <textarea name="content" class="form-control @error('content') is-invalid @enderror"
+                  placeholder="Inserisci il contenuto del post"></textarea>
+              </div>
+
+              <div class="form-group">
+                <label>Categoria</label>
+                <select name="category_id" class="form-control @error('content') is-invalid @enderror">
+                  <option value="" selected disabled hidden>Seleziona una categoria</option>
+                  @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">
+                      {{ $category->name }}
+                    </option>
+                  @endforeach
+                </select>
+              </div>
+
+              <div class="form-group">
+                <input type="submit" class="btn btn-success" value="Crea post">
+              </div>
+
+            </form>
+
+          </div>
+        </div>
+        <a href="{{ route('admin.posts.index') }}" class="btn btn-info mt-4">Indietro</a>
+      </div>
+    </div>
+  </div>
+@endsection
